4af497892d0c4b02c36f70c74a9922e2b8bd8ad6
twinearths <info@twinearths.co.jp>
2022-01-27 19:12:33 +0900

    add dark mode

--- SwiftUIApp/ContentView.swift
+++ SwiftUIApp/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var presentView: Bool = false
+    @State private var pushView: Bool = false
+    @State private var message: String = "Hello, world!"
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+        Text(message)
             .padding()
+            .navigationBarTitle("トップ画面", displayMode: .large)
+            .navigationBarHidden(false)
+            .onAppear {
+                let appearance = UINavigationBarAppearance()
+                appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+                appearance.backgroundColor = UIColor(Color.orange.opacity(0.8))
+                // Inline appearance (standard height appearance)
+                UINavigationBar.appearance().standardAppearance = appearance
+                // Large Title appearance
+                UINavigationBar.appearance().scrollEdgeAppearance = appearance
+            }
+            .navigationBarItems(
+                leading:
+                    Button(action: {
+                        self.presentView = true
+                    }) {
+                        Text("Present")
+                    }.sheet(isPresented: self.$presentView) {
+                        Text("Present a view")
+                    },
+                trailing:
+                    HStack {
+                        Button(action: {
+                            self.message = "Pushed trash button"
+                        }) {
+                            Image(systemName: "trash")
+                        }
+                        Button(action: {
+                            self.pushView = true
+                        }) {
+                            Text("Push")
+                        }
+                        NavigationLink(destination: Text("Push a view"), isActive:$pushView) {
+                        }
+                    }
+            )
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
