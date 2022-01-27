c219bedf6242dc29d2e04a6a4cb79604c1035df0
twinearths <info@twinearths.co.jp>
2022-01-27 13:33:56 +0900

    Add Xcode project

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by DAO QUAN on 2022/01/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
